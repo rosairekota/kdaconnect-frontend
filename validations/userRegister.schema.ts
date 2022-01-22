8893ab5cac84d833575dbc4637a8f67080a2f83c
rkota <rosairekota@gmail.com>
2022-01-22 20:31:34 +0100

    feat:configure yup and react-hook-form for inputs validations

--- validations/userRegister.schema.ts
+++ validations/userRegister.schema.ts
@@ -2,10 +2,10 @@ import * as yup from 'yup';
 
 export const userRegisterSchema = yup
   .object({
-    firstName: yup.string().required('Le Prenom est obligatoire'),
+    firstName: yup.string().required('Le Prénom est obligatoire'),
     lastName: yup.string().required('Le Postnom est obligatoire'),
-    userName: yup.string().required(),
-    email: yup.string().email().required(),
+    userName: yup.string().required('Le username est obligatoire'),
+    email: yup.string().email().required('Email est obligatoire'),
     password: yup.string().required(),
     age: yup.number().positive().integer().required(),
   })
